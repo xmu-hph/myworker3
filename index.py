3dfab3687dcb9f22be473447dd2abd6b8cfeede0
huph210950 <huph210950@gmail.com>
2024-04-27 16:37:42 +0800

    chose js

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
-def handleRequest(request):
-    return __new__(Response('Python Worker hello world!', {
-        'headers' : { 'content-type' : 'text/plain' }
-    }))
-
-addEventListener('fetch', (lambda event: event.respondWith(handleRequest(event.request))))
+export default {
+	fetch() {
+		return new Response('Hello worker!', {
+			headers: {
+				'content-type': 'text/plain',
+			},
+		});
+	},
+};
