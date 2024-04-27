17df7e049e3f6ce1ad3e7bf5309dc23cb9f5152c
huph210950 <huph210950@gmail.com>
2024-04-27 17:09:03 +0800

    new

--- index.py
+++ index.py
@@ -1,9 +1,8 @@
-export default {
-	fetch() {
-		return new Response('Hello worker!', {
-			headers: {
-				'content-type': 'text/plain',
-			},
-		});
-	},
-};
+from js import Response
+import numpy as np
+import pandas as pd
+
+async def on_fetch(request, env):
+    dates = pd.date_range("20240101", periods=6)
+    df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list("ABCD"))
+    return Response.new(f"Hello \n\n {df}")
